4c47c91a531db996a5f9411f855d6f3ef7df2895
yashwanth535 <yashwanth.lumia535@gmail.com>
2025-03-02 15:46:35 +0530

    "deployment error rectified"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Error from "./pages/error/error";
 import ProtectedRoute from "./components/ProtectedRoute";
-import LandingPage from "./pages/landing";
+import LandingPage from "./pages/Landing";
 import AddTransaction from "./components/TransactionForm";
 import fetchTransactions from "./components/TransactionList";
 import Home from "./pages/Home";
@@ -14,9 +14,6 @@ function App() {
         {/* Public Route */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/error" element={<Error/>} />
-        <Route path="/auth" element={<LandingPage />} />
-        <Route path="/add-transaction" element={<AddTransaction />} />
-        <Route path="/fetch-transactions" element={<fetchTransactions />} />
         <Route path="/home" element={<Home />} />
         <Route path="*" element={<NotFound  />} />
 
